d1be5542b97908a94615e742fc121d6dee624a2a
Nick Barnard <n.barnard@ctidigital.com>
2023-02-08 15:35:32 +0000

    Shrink textures

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,16 @@ function App() {
   return (
     <div className="App">
       <Scene />
+      <div
+        style={{
+          position: "fixed",
+          backgroundColor: "rgba(0,0,0,0.4)",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+        }}
+      />
       <MainContent />
     </div>
   );
